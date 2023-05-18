bbdb81b479abd60996f5cf6a6496f8b7d52ec0c5
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-05-18 22:22:41 +0330

    Create Author model in blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=255, blank=True, null=True)
