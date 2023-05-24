46e2d4ac799d882c35fce3d5bfe07f979c632870
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-05-24 21:13:42 +0330

    Create Subscription model

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,18 @@ User = get_user_model()
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="subscriptions"
+    )
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name="subscribers"
+    )
+
+    class Meta:
+        unique_together = ["subscriber", "author"]
+
+    def __str__(self):
+        return f"{self.subscriber} subscribed to {self.author}"
