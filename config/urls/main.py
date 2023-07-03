4bb9fe3764f6f7d94544293cf165bf1f9fbd8423
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-07-03 21:52:07 +0330

    Update db service in docker-compose

--- config/urls/main.py
+++ config/urls/main.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", include("config.urls.swagger")),
     path("admin/", admin.site.urls),
     path("auth/", include("users.urls")),
     path("auth/", include("config.urls.jwt")),
@@ -12,7 +13,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns = [
-        path("", include("config.urls.swagger")),
         path("__debug__/", include("debug_toolbar.urls")),
         *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
         *urlpatterns,
