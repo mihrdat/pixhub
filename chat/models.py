3d021d598874f4af0b14783dd4275c8e1edc7d3c
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-07-20 12:54:02 +0330

    Create Message model in chat app

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Message(models.Model):
+    content = models.TextField()
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="received_messages"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
