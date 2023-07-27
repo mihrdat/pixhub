4f3321ba65486494ad1ba7658ace77fbc1f701fa
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-07-27 11:43:04 +0330

    Edit in ChatPage and Message models

--- chat/models.py
+++ chat/models.py
@@ -5,21 +5,24 @@ User = get_user_model()
 
 
 class ChatPage(models.Model):
-    name = models.CharField(max_length=55, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="chat_pages")
+    contact = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.contact.email
 
 
 class Message(models.Model):
     content = models.TextField()
-    user = models.ForeignKey(
+    sender = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="sent_messages"
     )
     recipient = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="received_messages"
     )
-    chat_page = models.ForeignKey(
-        ChatPage, on_delete=models.SET_NULL, related_name="messages", null=True
-    )
+    seen = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"From {self.user.email} to {self.recipient.email}: {self.content}"
+        return f"From {self.sender.email} to {self.recipient.email}: {self.content}"
