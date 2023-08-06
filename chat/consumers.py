1df938fc53ff5ef2e9708d7ac6071947f52c8311
Mehrdad Mahmoodpoor <mahmoodpoor.mihrdat@gmail.com>
2023-08-06 15:14:36 +0330

    Define index view

--- chat/consumers.py
+++ chat/consumers.py
@@ -18,29 +18,22 @@ class ChatPageConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        print(message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
                 "message": message,
-                "user_id": text_data_json["user_id"],
-                "contact_id": text_data_json["contact_id"],
             },
         )
 
     async def chat_message(self, event):
         message = event["message"]
-        user_id = event["user_id"]
-        contact_id = event["contact_id"]
         await self.send(
             text_data=json.dumps(
                 {
                     "type": "chat",
                     "message": message,
-                    "user_id": user_id,
-                    "contact_id": contact_id,
                 }
             )
         )
--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 
+@api_view()
+@permission_classes([IsAuthenticated])
 def index(request):
-    contact_id = request.GET.get("contact_id", "Anonymous")
-    print(request.user)
-    return render(
-        request,
-        "chat/index.html",
-        {"contact_id": contact_id, "current_user": request.user},
-    )
+    contact_id = request.GET.get("contact_id")
+    if contact_id is None:
+        return Response({"detail": "Contact ID is not provided."})
+    return render(request, "chat/index.html")
